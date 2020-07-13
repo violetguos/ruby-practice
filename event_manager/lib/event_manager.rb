17a9b6949f21b8318845c480f30c1573b008034a
Y. Violet Guo <yueguo598@gmail.com>
2020-07-13 15:07:06 -0300

    First commit

---
+++ event_manager/lib/event_manager.rb
@@ -0,0 +1,68 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+require 'date'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def clean_phone(phone)
+  phone.gsub(/[^0-9]/, '').rjust(10, "0")[0..9]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts "EventManager initialized."
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
+counts = Hash.new(0)
+
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+
+  time = row[:regdate]
+  time = DateTime.strptime(time, '%m/%d/%y %H:%M')
+  # count which hour
+  counts[time.hour] +=1
+  zipcode = clean_zipcode(row[:zipcode])
+  phone = clean_phone(row[:homephone])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id,form_letter)
+end
+
+# find the most popular hour of registration
+p counts
+p counts.max_by{|k,v| v}
